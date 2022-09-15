8b946d0bd15a5ad49f3af9293b934f7494d4ffd1
evileveline <ferewella@gmail.com>
2022-09-15 18:04:27 +0300

    Работает только с EAGER, что могла, исправила

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -30,7 +30,7 @@ public class User implements UserDetails {
     @Column
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "users_id"),
             inverseJoinColumns = @JoinColumn(name = "roles_id"))
     private Set<Role> roles;
